2fee37b2edad11104c38650d3c845c17c0e830f3
Neerjara <55031473+Neerjara@users.noreply.github.com>
2026-04-02 06:31:38 +0530

    Update eks.tf

--- terraform/eks.tf
+++ terraform/eks.tf
@@ -1,20 +1,23 @@
 module "eks" {
   source  = "terraform-aws-modules/eks/aws"
-  version = "21.16.0"   # 👈 FIX
+  version = "21.16.0"
 
-  cluster_name    = "devsecops-cluster"
-  cluster_version = "1.29"
+  cluster_name = "devsecops-cluster"
+
+  kubernetes_version = "1.29"   # 👈 changed name
 
   vpc_id     = module.vpc.vpc_id
   subnet_ids = module.vpc.private_subnets
 
   eks_managed_node_groups = {
     default = {
-      desired_size = 2
-      max_size     = 3
-      min_size     = 1
-
       instance_types = ["t3.medium"]
+
+      scaling_config = {
+        desired_size = 2
+        max_size     = 3
+        min_size     = 1
+      }
     }
   }
 }
