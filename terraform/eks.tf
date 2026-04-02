2df38c37e493c0124e60bdeabd5f5fec192eb63e
Neerjara <55031473+Neerjara@users.noreply.github.com>
2026-04-02 06:27:50 +0530

    Update eks.tf

--- terraform/eks.tf
+++ terraform/eks.tf
@@ -1,8 +1,9 @@
 module "eks" {
-  source          = "terraform-aws-modules/eks/aws"
+  source  = "terraform-aws-modules/eks/aws"
+  version = "~> 19.0"   # 👈 FIX
+
   cluster_name    = "devsecops-cluster"
   cluster_version = "1.29"
-
 
   vpc_id     = module.vpc.vpc_id
   subnet_ids = module.vpc.private_subnets
